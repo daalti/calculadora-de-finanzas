e58bded067622161dc6386e58cb2381f24b465f7
Daniel Alonso Tirapu <daaltigo@gmail.com>
2025-02-04 14:36:59 +0100

    Improving the deploy in vercel

--- calculadora_finanzas/vite.config.ts
+++ calculadora_finanzas/vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor code into a separate chunk
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit
+  },
 });
